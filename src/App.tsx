1a55a5bce4156ad03b58ed52bd90e8debcc8413d
Timur <timur.mosenkov@gmail.com>
2024-02-15 22:16:20 +0200

    Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import Header from "./layout/Header"
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <Header />
   )
 }
 
---
+++ src/layout/Header.tsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+  return (
+    <header className="bg-slate-700 py-4 px-4">
+      <h2 className="text-zinc-200 text-xl">FDR</h2>
+    </header>
+  )
+}
+
+export default Header
